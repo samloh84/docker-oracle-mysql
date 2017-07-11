94b74eb253967c3dd1765436cb2bd630b0d11538
Samuel Loh <samuel.loh.yk@gmail.com>
2017-07-11 09:53:17 +0800

    Fixing entrypoint.sh

--- 5.7.18/centos7/entrypoint.sh
+++ 5.7.18/centos7/entrypoint.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+function initialize() {
 if [[ ! -d ${MYSQL_DATA_DIR} ]]; then
 	mkdir -p ${MYSQL_DATA_DIR}
 fi
 
 if [[ ! "$(ls -A ${MYSQL_DATA_DIR})" ]]; then
-	mysqld --initialize-insecure --datadir=${MYSQL_DATA_DIR} --user=${MYSQL_USERNAME}
+	echo "Initializing MySQL"
+	MYSQLD_INITIALIZE_ARGS+="--initialize-insecure "
+	MYSQLD_INITIALIZE_ARGS+="--basedir=${MYSQL_HOME} "
+	MYSQLD_INITIALIZE_ARGS+="--datadir=${MYSQL_DATA_DIR}"
+	mysqld ${MYSQLD_INITIALIZE_ARGS}
 fi
+}
 
-mysqld
-MYSQL_PID=$!
+function start() {
+	MYSQLD_ARGS+="--basedir=${MYSQL_HOME} "
+	MYSQLD_ARGS+="--datadir=${MYSQL_DATA_DIR}"
+	mysqld ${MYSQLD_ARGS} &
+	export MYSQL_PID=$!
+}
 
-trap kill -9 ${MYSQL_PID}
+function end() {
+kill -9 ${MYSQL_PID}
+}
+
+
+initialize
+trap end EXIT
+start
 wait ${MYSQL_PID}
